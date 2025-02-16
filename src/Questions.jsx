e217ee7a48baf6db9494e0c720ac9c35fba100d6
Bina <benjaminbinyam500@gmail.com>
2025-02-16 16:21:45 +0300

    single question at a time functionality

--- src/Questions.jsx
+++ src/Questions.jsx
@@ -1,11 +1,18 @@
 import Question from "./SingleQuestion";
 
-const Questions = ({ questions }) => {
+const Questions = ({ questions, activeId, toggleQuestion }) => {
   return (
     <section className="container">
       <h1>Questions</h1>
       {questions.map((question) => {
-        return <Question key={question.key} {...question} />;
+        return (
+          <Question
+            key={question.id}
+            {...question}
+            activeId={activeId}
+            toggleQuestion={toggleQuestion}
+          />
+        );
       })}
     </section>
   );

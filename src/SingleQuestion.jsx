6366e1c33a5d41c8bfb42d047addf815ab473ae2
Bina <benjaminbinyam500@gmail.com>
2025-02-16 15:38:15 +0300

    question component toggle functionality added

--- src/SingleQuestion.jsx
+++ src/SingleQuestion.jsx
@@ -3,8 +3,16 @@ import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
 const SingleQuestion = ({ title, info }) => {
   const [showInfo, setShowInfo] = useState(false);
-  return <article className="question">
-    
-  </article>;
+  return (
+    <article className="question">
+      <header>
+        <h5>{title} </h5>
+        <button className="question-btn" onClick={() => setShowInfo(!showInfo)}>
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {showInfo && <p>{info} </p>}
+    </article>
+  );
 };
 export default SingleQuestion;
